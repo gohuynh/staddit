8d9e8eb34ec197ef99b0730b2709058970c556c9
Asa Royal <oneroyalace@gmail.com>
2017-04-25 20:47:45 -0400

    added features to subreddit search page

--- staddit/stad/helper.py
+++ staddit/stad/helper.py
@@ -3,6 +3,7 @@
 from django.db import connection
 from nltk.tokenize import sent_tokenize
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+import numpy as np
 
 cur = connection.cursor()
 
@@ -32,6 +33,8 @@ def sentimentAnalysis(redditor):
 	sentences = []
 	avgScore = 0
 	scores = []
+	subtract = 0
+
 
 	analyzer = SentimentIntensityAnalyzer()
 	for c in comms:
@@ -40,10 +43,57 @@ def sentimentAnalysis(redditor):
 		thisScore = analyzer.polarity_scores(sen)['compound']
 		scores.append(thisScore)
 		avgScore += thisScore
+		if thisScore == 0:
+			subtract +=1
 
-	avgScore = avgScore/len(sentences)
+	if 0 != len(sentences):
+		avgScore = avgScore/(len(sentences)-subtract)
+	else:
+		avgScore = 0
 	minScore = sentences[scores.index(min(scores))], min(scores)
 	maxScore = sentences[scores.index(max(scores))], max(scores)
 
 	return avgScore, minScore, maxScore
 
+def mostPostedSub(subreddit):
+	"""return most post-igious users in a sub"""
+	cur.execute("""select author, count(author) from posted_in, posted_by where subreddit = '"""+subreddit+"""' and posted_by.id = posted_in.id and author != '[deleted]' group by author order by count(author) desc limit 3""")
+	return cur.fetchall()
+	
+def topScoringSub(subreddit):
+	"""return top scoring comment in a sub"""
+	cur.execute("""select body, author, score from posted_in, posted_by, comment where subreddit = '"""+subreddit+"""' and posted_by.id = posted_in.id and comment.id = posted_by.id and author not like '%AutoModerator%' order by score desc limit 1""")
+	return cur.fetchone()
+
+def sentimentAnalysisSub(subreddit):
+	"""return sentiment analysis summary stats for sub"""
+
+	cur.execute("""select body from posted_in, comment where posted_in.id = comment.id and posted_in.subreddit = '"""+subreddit+"""' limit 100""")
+
+	comms = cur.fetchall()
+	sentences = []
+	avgScore = 0
+	scores = []
+	subtract = 0
+
+	analyzer = SentimentIntensityAnalyzer()
+	for c in comms:
+		sentences.extend(sent_tokenize(c[0]))
+	for sen in sentences:
+		thisScore = analyzer.polarity_scores(sen)['compound']
+		scores.append(thisScore)
+		avgScore += thisScore
+		if thisScore == 0:
+			subtract +=1
+
+	newScores = [x for x in scores if x!=0]
+	
+	if 0 != len(sentences):
+		avgScore = avgScore/(len(sentences) - subtract)
+	else:
+		avgScore = 0
+		
+	medScore = np.median(np.array(newScores))
+	stdScore = np.std(np.array(newScores))
+
+	return avgScore, stdScore, medScore
